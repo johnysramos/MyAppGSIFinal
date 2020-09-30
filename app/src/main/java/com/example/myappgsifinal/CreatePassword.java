018fbf1f9a3a6968c95899126f39a7941d0ff7bf
tccjrl <tcc.utfpr.jrl@gmail.com>
2020-09-29 23:35:18 -0300

    Tela de criar senha concluída

--- app/src/main/java/com/example/myappgsifinal/CreatePassword.java
+++ app/src/main/java/com/example/myappgsifinal/CreatePassword.java
@@ -2,11 +2,12 @@ package com.example.myappgsifinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.TextView;
+
 
 public class CreatePassword extends AppCompatActivity {
 
@@ -20,6 +21,11 @@ public class CreatePassword extends AppCompatActivity {
         EditText pwd = (EditText) findViewById(R.id.pwd);
         TextView pwdView = (TextView) findViewById(R.id.pwdView);
         TextView result = (TextView) findViewById(R.id.result);
+        TextView hasLowerCaseTxView = (TextView) findViewById(R.id.hasLowerCaseTxView);
+        TextView hasUpperCaseTxView = (TextView) findViewById(R.id.hasUpperCaseTxView);
+        TextView hasNumTxView = (TextView) findViewById(R.id.hasNumTxView);
+        TextView hasSpecialCharTxView = (TextView) findViewById(R.id.hasSpecialCharTxView);
+        TextView hasMoreThan7TxView = (TextView) findViewById(R.id.hasMoreThan7TxView);
 
         boolean hasMoreThan7 = false;
         boolean hasLowerCase = false;
@@ -51,11 +57,65 @@ public class CreatePassword extends AppCompatActivity {
         pwdView.setText(pwd.getText());
 
         if (hasMoreThan7 && hasLowerCase && hasUpperCase && hasNum && hasSpecialChar) {
+            result.setText(R.string.valid_pwd);
+            hasLowerCaseTxView.setText(R.string.hasLowerCase);
+            hasUpperCaseTxView.setText(R.string.hasUpperCase);
+            hasNumTxView.setText(R.string.hasNum);
+            hasSpecialCharTxView.setText(R.string.hasSpecialChar);
+            hasMoreThan7TxView.setText(R.string.hasMoreThan7);
 
-            result.setText(String.format(getResources().getString(R.string.valid_pwd)
-             + getResources().getString(R.string.hasNum)));
+            result.setTextColor(0xFF007F00);
+            hasLowerCaseTxView.setTextColor(0xFF007F00);
+            hasUpperCaseTxView.setTextColor(0xFF007F00);
+            hasNumTxView.setTextColor(0xFF007F00);
+            hasSpecialCharTxView.setTextColor(0xFF007F00);
+            hasMoreThan7TxView.setTextColor(0xFF007F00);
         }
-        else result.setText(R.string.invalid_pwd);
+        else {
+            result.setText(R.string.invalid_pwd);
+            hasLowerCaseTxView.setText(R.string.hasLowerCase);
+            hasUpperCaseTxView.setText(R.string.hasUpperCase);
+            hasNumTxView.setText(R.string.hasNum);
+            hasSpecialCharTxView.setText(R.string.hasSpecialChar);
+            hasMoreThan7TxView.setText(R.string.hasMoreThan7);
+
+            result.setTextColor(0xCCA30000);
+
+            if (hasLowerCase) {
+                hasLowerCaseTxView.setTextColor(0xFF007F00);
+            }
+            else {
+                hasLowerCaseTxView.setTextColor(0xCCA30000);
+            }
+
+            if (hasUpperCase) {
+                hasUpperCaseTxView.setTextColor(0xFF007F00);
+            }
+            else {
+                hasUpperCaseTxView.setTextColor(0xCCA30000);
+            }
+
+            if (hasNum) {
+                hasNumTxView.setTextColor(0xFF007F00);
+            }
+            else {
+                hasNumTxView.setTextColor(0xCCA30000);
+            }
+
+            if (hasSpecialChar) {
+                hasSpecialCharTxView.setTextColor(0xFF007F00);
+            }
+            else {
+                hasSpecialCharTxView.setTextColor(0xCCA30000);
+            }
+
+            if (hasMoreThan7) {
+                hasMoreThan7TxView.setTextColor(0xFF007F00);
+            }
+            else {
+                hasMoreThan7TxView.setTextColor(0xCCA30000);
+            }
+        }
     }
 
 }
